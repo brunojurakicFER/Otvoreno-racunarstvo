6884d0619524a4ba6aec104bac7a0867168970fa
brunojurakic <bruno.jurakic@fer.unizg.hr>
2025-01-18 16:51:58 +0100

    Revert "add frontend auth"
    This reverts commit 1168082088a102bae562aab635b2a1b239469c77.

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,30 +1,19 @@
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
+import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom';
 import Index from "./Index.jsx";
 import Datatable from "./Datatable.jsx";
 import Header from "./Header.jsx";
-import authConfig from '../../auth_config.json';
 import '../Styles/App.css';
 
 function App() {
   return (
-    <Auth0Provider
-      domain={authConfig.domain}
-      clientId={authConfig.clientId}
-      authorizationParams={{
-        redirect_uri: 'http://localhost:5173/index',
-        audience: authConfig.audience
-      }}
-    >
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Navigate to="/index" />} />
-          <Route path="/index" element={<Index />} />
-          <Route path="/datatable" element={<Datatable />} />
-        </Routes>
-      </Router>
-    </Auth0Provider>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/index" />} />
+        <Route path="/index" element={<Index/>} />
+        <Route path="/datatable" element={<Datatable/>} />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,36 +1,11 @@
 import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
 import '../Styles/Header.css';
 
 const Header = () => {
-  const { loginWithRedirect, user, isAuthenticated, logout, getAccessTokenSilently } = useAuth0();
-
-  const fetchUserData = async () => {
-    try {
-      const token = await getAccessTokenSilently();
-      const response = await fetch('http://localhost:3000/user', {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error('Error fetching user data:', error);
-    }
-  };
-
   return (
     <header className="header border-b border-white mb-3">
       <div className="header-title cursor-pointer" onClick={() => window.location.href = '/index'}>Otvoreno računarstvo</div>
-      {isAuthenticated ? (
-        <>
-          <button className="header-login-btn bg-transparent" onClick={fetchUserData}>Fetch User Data</button>
-          <button className="header-login-btn bg-transparent" onClick={() => logout({ returnTo: window.location.origin })}>Log Out</button>
-        </>
-      ) : (
-        <button className="header-login-btn bg-transparent" onClick={() => loginWithRedirect()}>Log In</button>
-      )}
+      <button className="header-login-btn bg-transparent">Log In</button>
     </header>
   );
 };
