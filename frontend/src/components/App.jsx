1168082088a102bae562aab635b2a1b239469c77
brunojurakic <bruno.jurakic@fer.unizg.hr>
2025-01-18 16:29:58 +0100

    add frontend auth

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,19 +1,30 @@
-import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import Index from "./Index.jsx";
 import Datatable from "./Datatable.jsx";
 import Header from "./Header.jsx";
+import authConfig from '../../auth_config.json';
 import '../Styles/App.css';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Navigate to="/index" />} />
-        <Route path="/index" element={<Index/>} />
-        <Route path="/datatable" element={<Datatable/>} />
-      </Routes>
-    </Router>
+    <Auth0Provider
+      domain={authConfig.domain}
+      clientId={authConfig.clientId}
+      authorizationParams={{
+        redirect_uri: 'http://localhost:5173/index',
+        audience: authConfig.audience
+      }}
+    >
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navigate to="/index" />} />
+          <Route path="/index" element={<Index />} />
+          <Route path="/datatable" element={<Datatable />} />
+        </Routes>
+      </Router>
+    </Auth0Provider>
   );
 }
 
