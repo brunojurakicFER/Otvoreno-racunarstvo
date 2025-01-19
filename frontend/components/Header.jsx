26612cdf7a7a382e4537c47ae8fd807e2825ce21
brunojurakic <bruno.jurakic@fer.unizg.hr>
2025-01-19 01:37:08 +0100

    Revert "add toast"
    This reverts commit 6d4adc157ca20bae85adf28b540c78473ac4a846.

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -2,10 +2,8 @@
 
 import React from 'react';
 import {useUser} from "@auth0/nextjs-auth0/client";
-import '@/Styles/Header.css';
+import '@/Styles/Header.css'
 import axios from "axios";
-import {ToastContainer, toast} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 const Header = () => {
   const {user, error, isLoading} = useUser();
@@ -14,60 +12,55 @@ const Header = () => {
     try {
       const res = await axios.get('http://localhost:10000/refresh-data', {
         headers: {
-          'user': user
+          'user' : user
         }
-      });
-      toast.success('Data refreshed successfully!');
-      console.log(res.data);
+      })
+      console.log(res.data)
     } catch (e) {
-      toast.error('Failed to refresh data.');
-      console.error(e);
+      console.error(e)
     }
   }
 
   return (
-    <>
-      <header className="header border-b border-white mb-3">
-        <div className="header-title cursor-pointer" onClick={() => window.location.href = '/index'}>
-          Otvoreno računarstvo
-        </div>
+    <header className="header border-b border-white mb-3">
+      <div className="header-title cursor-pointer" onClick={() => window.location.href = '/index'}>
+        Otvoreno računarstvo
+      </div>
 
-        {isLoading ? (
-          <div>Loading...</div>
-        ) : error ? (
-          <div>Error: {error.message}</div>
-        ) : user ? (
-          <div className={'gap-5 flex-row flex'}>
-            <button
-              className="header-logout-btn bg-transparent"
-              onClick={() => window.location.href = '/profile'}
-            >
-              Korisnički profil
-            </button>
-            <button
-              className="header-logout-btn bg-transparent"
-              onClick={handleRefresh}
-            >
-              Osvježi preslike
-            </button>
-            <button
-              className="header-logout-btn bg-transparent"
-              onClick={() => window.location.href = '/api/auth/logout'}
-            >
-              Log Out
-            </button>
-          </div>
-        ) : (
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>Error: {error.message}</div>
+      ) : user ? (
+        <div className={'gap-5 flex-row flex'}>
           <button
             className="header-logout-btn bg-transparent"
-            onClick={() => window.location.href = '/api/auth/login'}
+            onClick={() => window.location.href = '/profile'}
           >
-            Log In
+            Korisnički profil
           </button>
-        )}
-      </header>
-      <ToastContainer position="bottom-right"/>
-    </>
+          <button
+            className="header-logout-btn bg-transparent"
+            onClick={handleRefresh}
+          >
+            Osvježi preslike
+          </button>
+          <button
+            className="header-logout-btn bg-transparent"
+            onClick={() => window.location.href = '/api/auth/logout'}
+          >
+            Log Out
+          </button>
+        </div>
+      ) : (
+        <button
+          className="header-logout-btn bg-transparent"
+          onClick={() => window.location.href = '/api/auth/login'}
+        >
+          Log In
+        </button>
+      )}
+    </header>
   );
 };
 
